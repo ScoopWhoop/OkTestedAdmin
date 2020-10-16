f26b729dd22807befc422d4652a8dd0df7e6f7dd
aviguptaSW <avi.gupta@scoopwhoop.com>
2020-10-16 17:33:35 +0530

    done

--- src/main/java/com/OKTAdmin/base/TestBase.java
+++ src/main/java/com/OKTAdmin/base/TestBase.java
@@ -51,9 +51,9 @@ public class TestBase {
 			
 	         //WebDriverManager.chromedriver().setup();
 			//System.setProperty(ChromeDriverService.CHROME_DRIVER_SILENT_OUTPUT_PROPERTY, "false");
-			System.setProperty("webdriver.chrome.driver", "E:\\Automation_Tools\\chromedriver_win32 (1)\\chromedriver.exe");
+			//System.setProperty("webdriver.chrome.driver", "E:\\Automation_Tools\\chromedriver_win32 (1)\\chromedriver.exe");
 
-			//System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+			System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
 	       // ChromeOptions options = new ChromeOptions();
 	       // options.addArguments("--headless");
 	         // Must maximize Chrome by `start-maximized`
